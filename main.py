901821e56c113554f4f861c1cbcc1e0f0cfb3b06
nbarnett19 <natalie.barnett@stud.hslu.ch>
2024-07-20 22:36:12 +0200

    Repo setup

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 # main.py
 
+import os
+from dotenv import load_dotenv
+import langchain
+import langchain_core
+import langchain_community
 from transcript_loader import TranscriptLoader
 from model_manager import ModelManager
 from prompting_methods import ZeroShotPrompt, FewShotPrompt, ChainOfThoughtPrompt
 
+# gemini_api_key = os.getenv('GEMINI_API_KEY')
+# ChatGoogleGenerativeAI(model=model_choice, temperature=0.7, google_api_key=gemini_api_key, top_p=0.3)
+
 def main():
     # Define file path and research questions
     file_path = "./Data/Alle_Transkripte_EN.pdf"
-    rqs = """Explore and describe experiences of internal medicine doctors after wearing a 
+    rqs = """Explore and describe experiences of internal medicine doctors after wearing a
     glucose sensor with focus on two research questions:
     1. How can self-tracking with a glucose sensor influence residents’ understanding of glucose metabolism?
-    2. How can self-tracking with a glucose sensor improve residents’ awareness, appreciation, and 
+    2. How can self-tracking with a glucose sensor improve residents’ awareness, appreciation, and
     understanding of patients with diabetes?"""
 
     # Choose model and method for testing
@@ -33,7 +41,7 @@ def main():
         prompt = FewShotPrompt(connect.llm)
         results = prompt.generate_response(data, rqs, examples)
     elif method == 'chain-of-thought':
-        chain_template = """You are a researcher. Based on the research questions: {rqs} 
+        chain_template = """You are a researcher. Based on the research questions: {rqs}
         and data: {data}, generate themes with definitions and supporting quotes."""
         prompt = ChainOfThoughtPrompt(connect.llm)
         results = prompt.generate_response(data, rqs, chain_template)
