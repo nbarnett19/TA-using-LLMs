ee7d9471e8dcb42d5f75327e21ba2c4814e7ad4f
nbarnett19 <natalie.barnett@stud.hslu.ch>
2024-07-21 18:50:33 +0200

    update methods

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 # Importing necessary classes to make them accessible at the package level
 
 from .model_manager import ModelManager
-from .prompting_methods import ZeroShotPrompt, FewShotPrompt, ChainOfThoughtPrompt
 from .transcript_loader import TranscriptLoader
 from .rag_integration import RAGIntegration
 from .JSONparser import CodeExcerpt, ZSThemes
+from prompting_methods import ThematicAnalysis
