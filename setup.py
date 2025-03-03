be99984851bda1ff0ee7a45088a38e2c0d681d8d
nbarnett19 <natalie.barnett@stud.hslu.ch>
2025-03-03 15:04:16 +0100

    add toml

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="TA-using-LLMs",
     version="0.0.1",
-    author="Natalie Barnett",
+    authors="Natalie Barnett",
     author_email="nataliebarnett.ch@gmail.com",
     description="An application that performs qualitative thematic analysis using LLMs",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entrypoints={"console_scripts":["TA_using_LLMs = src.main:main"]},
+    entrypoints={"console_scripts": ["TA_using_LLMs = src.main:main"]},
+    homepage="https://github.com/nbarnett19/TA-using-LLMs",
 )
